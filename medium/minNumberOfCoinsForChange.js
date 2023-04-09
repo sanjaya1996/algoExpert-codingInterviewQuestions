69da4d003f72ad57560413afc70f50aba247fc9f
Sanzay Dahal <sanzaydahal12345@gmail.com>
2023-04-09 15:59:05 +1000

    Fix minNumberOfCoinsForChange

--- medium/minNumberOfCoinsForChange.js
+++ medium/minNumberOfCoinsForChange.js
@@ -1,10 +1,9 @@
 // O(nd) Time | O(n) Space, Complexity
 
 function minNumberOfCoinsForChange(n, denoms) {
-  // Write your code here.
-  let nums = [...Array(n + 1).keys()];
-
-  for (const denom of denoms) {
+  let nums = new Array(n + 1).fill(Infinity);
+  nums[0] = 0;
+  for (denom of denoms) {
     for (let amount = 1; amount <= nums.length - 1; amount++) {
       if (denom <= amount) {
         nums[amount] = Math.min(nums[amount], 1 + nums[amount - denom]);
@@ -12,5 +11,5 @@ function minNumberOfCoinsForChange(n, denoms) {
     }
   }
 
-  return nums[n];
+  return nums[n] === Infinity ? -1 : nums[n];
 }
